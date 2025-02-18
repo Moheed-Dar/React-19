6bdecb2aa4bb11cb579c939ad3612128b4d7c93f
Moheed-Dar <moheeddar324@gmail.com>
2025-02-18 16:53:41 +0500

    jsx props all

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { useState } from 'react';
 import User from './User';
+import Student from './Student'
+import Wrapper from './Wrapper'
 function App() {
-    let userobj ={
+    let userobje ={
+      name : 'moheeddar',
+      class: 'bsit',
+      rollnum : 10,
+      session: 2021-2025,
+      depart: 'bsit'
+    };
+    let userobjc ={
+      name : 'moheeddars',
+      class: 'bsits',
+      rollnum : 102,
+      session: 2021-2025,
+      depart: 'bsits'
+    };
+    let userobja= {
       name : 'moheeddar',
       class: 'bsit',
       rollnum : 10,
       session: 2021-2025,
       depart: 'bsit'
     }
+     let collagename = [ 'let','const','delta','omigla']
+
+     const [student,setStudent] = useState()
+  
   return (
     <>
       <h2>props in react 19 </h2>
-      <User user={userobj} />
+      {
+        student && <Student name={student}/>
+      }
+      <button onClick={()=>setStudent("moheed dar")} >update the data</button>
+      <Student  name={collagename[0]} />
+      <User user={userobje}/>
+      <User user={userobjc}/>
+      <User user={userobja}/>   
+      <Wrapper>
+        <h2>moheed dar</h2>
+      </Wrapper>
     </>
   );
 }
--- src/User.jsx
+++ src/User.jsx
@@ -4,8 +4,11 @@ function User({user}) {
     return (
         <div>
             <h1>Name:{user.name}</h1>
-            <h1>Age:{user.age}</h1>
+            <h1>Age:{user.class}</h1>
+            <h1>Name:{user.rollnum}</h1>
+            <h1>Age:{user.session}</h1>
             <h2  className='text-xl uppercase' >Department:{user.depart}</h2>
+            <hr/>
         </div>
 
     )
