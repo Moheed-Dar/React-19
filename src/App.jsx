4116b6701e65ed68ad92cdf6230ccf55924b15fb
Moheed-Dar <moheeddar324@gmail.com>
2025-02-21 20:56:10 +0500

    loop in JSX with map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,119 @@
-import { useState } from 'react';
+import { useState } from "react";
+
 function App() {
-  const [country,setCountry] =useState( "pak")
+  const userData = [
+    {
+      name: "moheed dar",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 10,
+    },
+    {
+      name: "noman khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 9,
+    },
+    {
+      name: "daod khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 11,
+    },
+    {
+      name: "shaheen",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 35,
+    },
+    {
+      name: "daod khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 11,
+    },
+    {
+      name: "shaheen",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 35,
+    },
+    {
+      name: "daod khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 11,
+    },
+    {
+      name: "shaheen",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 35,
+    },
+    {
+      name: "daod khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 11,
+    },
+    {
+      name: "shaheen",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 35,
+    },
+    {
+      name: "daod khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 11,
+    },
+    {
+      name: "shaheen",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 35,
+    },
+    {
+      name: "daod khan",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 11,
+    },
+    {
+      name: "shaheen",
+      session: "2021-2025",
+      department: "BSIT",
+      rollnum: 35,
+    },
+  ];
+
   return (
     <>
-        <h1>select city:{country} </h1>
-        <select onChange={(e)=>setCountry(e.target.value)} >
-          <option  >Muzaffarabad</option>
-          <option>Muzaffarghar</option>
-          <option>pindi</option>
-          <option>noshahrah</option>
-        </select>
+      <h1>Loop in JSX with map function</h1>
+      <table className="border-collapse border border-black w-full">
+        <thead>
+          <tr className="border border-black">
+            <th className="border border-black p-2">Name</th>
+            <th className="border border-black p-2">Session</th>
+            <th className="border border-black p-2">Department</th>
+            <th className="border border-black p-2">Roll Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user, index) => (
+            <tr key={index} className="border border-black">
+              <td className="border border-black p-2">{user.name}</td>
+              <td className="border border-black p-2">{user.session}</td>
+              <td className="border border-black p-2">{user.department}</td>
+              <td className="border border-black p-2">{user.rollnum}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </>
   );
 }
+
 export default App;
