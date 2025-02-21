39960b02ff74ce71e119a9a8a039b3856a19c58c
Moheed-Dar <moheeddar324@gmail.com>
2025-02-21 15:00:19 +0500

    form data handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
 function App() {
-  const [val, setVal]=useState(" ")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <>
-      <h2>props in react 19 </h2>
-      <input value={val}  placeholder="Enter some text" type='text' onChange={(e)=>setVal(e.target.value)}/>
-      <h1>The value is : {val}</h1>
-      <butto  onClick={()=>setVal("")} >Clear</butto>
-      
+      <form action="" method="get"  >
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder=" Enter your username" />
+        <br /><br />
+
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder=" Enter your Email " />
+        <br /><br />
+
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder=" Enter your password" />
+        <br /><br />
+
+        <button className='me-2' >submit</button>
+        <button onClick={() => { setName(" "); setEmail(" "); setPassword(" ") }} >Clear</button>
+
+        <h1>my username is :{name}</h1>
+        <h1>my email is :{email}</h1>
+        <h1>my password is :{password}</h1>
+
+      </form>
     </>
   );
 }
