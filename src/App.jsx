c33f9cda6f60e1ae74a57e888c9ddc651e0c5efa
Moheed-Dar <moheeddar324@gmail.com>
2025-02-21 16:01:33 +0500

    checked box handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { useState } from 'react';
 function App() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [skills,setSkills] = useState([]);
+  const handleskills=(event)=>{
+    console.log(event.target.value,event.target.checked);
+    if(event.target.checked){
+      setSkills([...skills,event.target.value]);
+    }else{
+      setSkills([...skills.filter((item)=>item!=event.target.value)])
+    }
+  }
+
   return (
     <>
-      <form action="" method="get"  >
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder=" Enter your username" />
-        <br /><br />
-
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder=" Enter your Email " />
-        <br /><br />
-
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder=" Enter your password" />
-        <br /><br />
-
-        <button className='me-2' >submit</button>
-        <button onClick={() => { setName(" "); setEmail(" "); setPassword(" ") }} >Clear</button>
-
-        <h1>my username is :{name}</h1>
-        <h1>my email is :{email}</h1>
-        <h1>my password is :{password}</h1>
-
-      </form>
+      <input onChange={handleskills}  type="checkbox" id="v1" value="javascript"  />
+      <label htmlFor="v1" >javascript</label>
+      <br/><br/>
+      <input onChange={handleskills} type="checkbox" id="v2" value="C++"  />
+      <label htmlFor="v2" >C++</label>
+      <br/><br/>
+      <input onChange={handleskills} type="checkbox" id="v3" value="Java" />
+      <label htmlFor="v3" >java</label>
+      <br/><br/>
+      <input onChange={handleskills} type="checkbox" id="v4"value="Python"  />
+      <label htmlFor="v4" >python</label>
+      <br/><br/>
+      <h1>{skills.toString()}</h1>
     </>
   );
 }
