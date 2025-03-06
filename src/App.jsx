fdee1231b9c069a9204b2387a8b660b63886b808
Moheed-Dar <moheeddar324@gmail.com>
2025-03-06 13:03:52 +0500

    pure component in react-19

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState, useTransition } from 'react';
+import React from 'react'
 
 function App() {
-    const [pending, startTransition] = useTransition();
-    const [text, setText] = useState("Click Here");
-    const [loading, setLoading] = useState(false); // <-- Track loading manually
-    const handleButton = () => {
-        setLoading(true); // Start loading
-        startTransition(() => {
-            setText("");
-        });
-        setTimeout(() => {
-            setText("Completed");
-            setLoading(false); // Stop loading after 2 sec
-        }, 2000);
-    };
-    return (
-        <div>
-            <h1>useTransition Hook</h1>
-            {loading ? ( // <-- Use loading state instead of pending
-                <img className="w-40 h-40  "
-                    src="https://assets-v2.lottiefiles.com/a/8e3a9f7c-1179-11ee-8831-e341af8fad88/3IMPKd5CHr.gif"
-                    alt="Loading..."
-                />
-            ) : null}
-            <button disabled={loading} onClick={handleButton}>
-                {text}
-            </button>
-        </div>
-    );
+  return (
+    <div>
+        <h1>pure Components in React-19</h1>
+        <Student  student={25} name="bscs" />
+        <Student  student={15} name="bsit" />
+    </div>
+  )
+}
+const Student=({student,name})=>{
+    let count=student;
+    
+    return(
+        <h1>there are {count } students are study in {name}.</h1>
+    )
 }
 
-export default App;
+export default App
