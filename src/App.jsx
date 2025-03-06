150242cc184b68bfb1537ad9a7058b093669f784
Moheed-Dar <moheeddar324@gmail.com>
2025-03-06 17:41:32 +0500

    Updating Object in State in React-19

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import {useState} from 'react'
-import Add from './Add'
-import Display from './Display'
+
 
 function App() {
-    const [input,setInput]=useState('')
+    const [data,setData] =useState({
+        name:'moheed dar',
+        address:{
+            city:'',
+            country:'pakistan'
+        }
+    })
+    const handlename=(val)=>{
+        data.name=val
+        setData({...data})
+    }
+    const handlecity=(val)=>{
+        data.address.city=val
+        setData({...data,address:{...data.address,city}})
+    }
   return (
     <div>
-        <Add  setInput={setInput} />
-        <Display  Display={input} />
+        <h1> Updating Objects in State </h1>
+        <hr/>
+        <input  onChange={(event)=>handlename(event.target.value)}  type="text"  placeholder="Enter your name" />
+        <h1>Name: {data.name} </h1>
+        <hr/>
+        <input  onChange={(event)=>handlecity(event.target.value)}  type="text"  placeholder="Enter your name" />
+        <h1>City: {data.address.city} </h1>
 
     </div>
   )
