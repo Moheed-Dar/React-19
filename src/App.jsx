90c39938deb4b9d62f28417f315a98d88d84dfe0
Moheed-Dar <moheeddar324@gmail.com>
2025-03-06 13:33:37 +0500

    Drived State in React-19

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-
+import { useState } from 'react'
 function App() {
-  return (
-    <div>
-        <h1>pure Components in React-19</h1>
-        <Student  student={25} name="bscs" />
-        <Student  student={15} name="bsit" />
-    </div>
-  )
-}
-const Student=({student,name})=>{
-    let count=student;
-    
-    return(
-        <h1>there are {count } students are study in {name}.</h1>
+    return (
+        <div>
+            <h1 className=' text-center text-3xl' >Drived State in React-19</h1>
+            <Drived />
+        </div>
     )
 }
-
+const Drived = () => {
+    const [users, setUsers] = useState([]);
+    const [user, setUser] = useState('');
+    const handleAddUsers = () => {
+        setUsers([...users, user])
+    }
+    const total = users.length;
+    const last = users[users.length - 1];
+    const unique = [...new Set(users)].length;
+    return (
+        <>
+            <h2> Total User : {total} </h2>
+            <h2> Last user: {last}  </h2>
+            <h2> Total Unique Users :  {unique} </h2>
+            <div className="text-center" >
+                <input onChange={(event) => setUser(event.target.value)} type="text" placeholder="add new user" />
+                <button onClick={handleAddUsers} >Update</button>
+            </div>
+            {users.map((item, index) => (
+                <h4 key={index} >{item} </h4>
+            ))}
+        </>
+    )
+}
 export default App
