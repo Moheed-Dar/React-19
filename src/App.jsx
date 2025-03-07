d8d8e55f6944aee0513d44e596c0c985a1321bf0
Moheed-Dar <moheeddar324@gmail.com>
2025-03-07 14:57:04 +0500

    useId 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import {useState} from 'react'
-
+import {useId} from 'react'
 
 function App() {
-    const [data,setData] =useState({
-        name:'moheed dar',
-        address:{
-            city:'',
-            country:'pakistan'
-        }
-    })
-    const handlename=(val)=>{
-        data.name=val
-        setData({...data})
-    }
-    const handlecity=(val)=>{
-        data.address.city=val
-        setData({...data,address:{...data.address,city}})
-    }
+  const email= useId()
+  const name= useId()
   return (
     <div>
-        <h1> Updating Objects in State </h1>
-        <hr/>
-        <input  onChange={(event)=>handlename(event.target.value)}  type="text"  placeholder="Enter your name" />
-        <h1>Name: {data.name} </h1>
-        <hr/>
-        <input  onChange={(event)=>handlecity(event.target.value)}  type="text"  placeholder="Enter your name" />
-        <h1>City: {data.address.city} </h1>
-
+      <label htmlFor={email} >Enter your Email</label>
+      <input id={email}  type ="text" placeholder="Enter your email"/>
+      <label htmlFor={name} >Enter your Email</label>
+      <input id={name}  type ="text" placeholder="Enter your email"/>
     </div>
   )
 }
